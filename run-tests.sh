fe9d6e05117516ecd77144b3b4c3d6ade1d34b30
Tibor Šimko <tibor.simko@cern.ch>
2026-03-12 14:16:25 +0100

    ci(run-tests): add usage help and refactor options (#100)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # This file is part of REANA.
-# Copyright (C) 2020, 2023, 2024 CERN.
+# Copyright (C) 2018, 2019, 2020, 2023, 2024, 2025, 2026 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,14 +9,19 @@
 set -o errexit
 set -o nounset
 
-# Enable globstar (**)
-shopt -s globstar
+docker_build() {
+    docker build -t docker.io/reanahub/wwwreanaio .
+}
 
-check_commitlint () {
+lint_commitlint() {
     from=${2:-master}
     to=${3:-HEAD}
     pr=${4:-[0-9]+}
-    npx commitlint --from="$from" --to="$to"
+    if command -v commitlint >/dev/null 2>&1; then
+        commitlint --from="$from" --to="$to"
+    else
+        npx commitlint --from="$from" --to="$to"
+    fi
     found=0
     while IFS= read -r line; do
         commit_hash=$(echo "$line" | cut -d ' ' -f 1)
@@ -46,41 +51,51 @@ check_commitlint () {
     fi
 }
 
-check_shellcheck () {
-    find . -name "*.sh" -exec shellcheck {} \+
+lint_docstyle() {
+    find templates -name "*.html" -exec awesome_bot --allow-dupe --skip-save-results --allow-redirect --white-list https://reana.cern.ch,https://twitter.com/reanahub/lists/reana-developers,https://indico.ific.uv.es,https://indico.desy.de -- {} +
 }
 
-check_docstyle () {
-    awesome_bot --allow-dupe --skip-save-results --allow-redirect --white-list https://reana.cern.ch,https://twitter.com/reanahub/lists/reana-developers,https://indico.ific.uv.es,https://indico.desy.de -- templates/**/*.html
-}
-
-check_dockerfile () {
+lint_hadolint() {
     docker run -i --rm docker.io/hadolint/hadolint:v2.12.0 < Dockerfile
 }
 
-check_docker_build () {
-    docker build -t docker.io/reanahub/wwwreanaio .
+lint_shellcheck() {
+    find . -name "*.sh" -exec shellcheck {} \+
 }
 
-check_all () {
-    check_commitlint
-    check_shellcheck
-    check_docstyle
-    check_dockerfile
-    check_docker_build
+all() {
+    docker_build
+    lint_commitlint
+    lint_docstyle
+    lint_hadolint
+    lint_shellcheck
+}
+
+help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  --all              Perform all checks [default]"
+    echo "  --docker-build     Check Docker build"
+    echo "  --help             Display this help message"
+    echo "  --lint-commitlint  Check linting of commit messages"
+    echo "  --lint-docstyle    Check linting of documentation"
+    echo "  --lint-hadolint    Check linting of Dockerfiles"
+    echo "  --lint-shellcheck  Check linting of shell scripts"
 }
 
 if [ $# -eq 0 ]; then
-    check_all
+    all
     exit 0
 fi
 
 arg="$1"
 case $arg in
-    --check-commitlint) check_commitlint "$@";;
-    --check-shellcheck) check_shellcheck;;
-    --check-docstyle) check_docstyle;;
-    --check-dockerfile) check_dockerfile;;
-    --check-docker-build) check_docker_build;;
-    *) echo "[ERROR] Invalid argument '$arg'. Exiting." && exit 1;;
+--all) all ;;
+--help) help ;;
+--docker-build) docker_build ;;
+--lint-commitlint) lint_commitlint "$@" ;;
+--lint-docstyle) lint_docstyle ;;
+--lint-hadolint) lint_hadolint ;;
+--lint-shellcheck) lint_shellcheck ;;
+*) echo "[ERROR] Invalid argument '$arg'. Exiting." && help && exit 1 ;;
 esac
